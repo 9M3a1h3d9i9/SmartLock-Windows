174a438f433182ab2531d3089b110b602f257f38
Mahdi313 <91673996+9M3a1h3d9i9@users.noreply.github.com>
2026-09-06 01:42:45 +0330

    feat: start Windows session monitoring

--- src/SmartLock.App/App.xaml.cs
+++ src/SmartLock.App/App.xaml.cs
@@ -1,5 +1,7 @@
 using System.Windows;
+using SmartLock.App.Services;
 using SmartLock.Core.Services;
+using SmartLock.Infrastructure.Windows;
 
 namespace SmartLock.App;
 
@@ -14,5 +16,10 @@ public partial class App : Application
         var window = new MainWindow(SecurityEvents);
         MainWindow = window;
         window.Show();
+
+        var monitor = new SessionContextMonitor(new WindowsSessionSignalProvider(), TimeSpan.FromSeconds(1));
+        window.Closed += (_, _) => monitor.Dispose();
+        monitor.ContextUpdated += (_, context) => window.UpdateSessionContext(context);
+        monitor.Start();
     }
 }
